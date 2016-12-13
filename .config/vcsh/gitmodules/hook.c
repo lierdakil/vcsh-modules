bdf6c9dc28cdc21be54bb931eb10de07e1010228
Nikolay Yakimov <root@livid.pp.ru>
2016-12-13 20:21:03 +0300

    Autocommit

--- .config/vcsh/gitmodules/hook.c
+++ .config/vcsh/gitmodules/hook.c
@@ -7,22 +7,42 @@
 #include <stdarg.h>
 #include <limits.h>
 
-static const char* subst(const char* path) {
+static void subst(const char* func, const char* path, char** result, int *should_free) {
+    *should_free = 0;
+    *result = path;
+
     char *workdir = getenv("VCSH_BASE");
     if(!workdir)
         workdir = getenv("HOME");
 
     if (workdir) {
-        char *gitmodules = getenv("VCSH_GITMODULES");
+        char *reponame = getenv("VCSH_REPO_NAME");
+        if(!reponame) return;
+        char *gitmodules = malloc(PATH_MAX+1);
+        strcpy(gitmodules, workdir);
+        strcat(gitmodules, "/.gitmodules.d/");
+        strcat(gitmodules, reponame);
         char modulespath[PATH_MAX+1];
         strcpy(modulespath, workdir);
         strcat(modulespath, "/.gitmodules");
-        if(gitmodules && strcmp(path, modulespath) == 0) {
-            return gitmodules;
+        //fprintf(stderr, "MODULESPATH: %s %s %s\n", func, path, gitmodules);
+        if(gitmodules && (strcmp(path, modulespath) == 0 || strcmp(path, ".gitmodules") == 0)) {
+            *result = gitmodules;
+            *should_free = 1;
+            return;
         }
     }
-    return path;
 }
+
+#define SUBST(ret_t, f_name, fargt, farg, path) \
+    static ret_t (*original_##f_name) fargt; \
+    if(!original_##f_name) original_##f_name = dlsym(RTLD_NEXT, #f_name); \
+    int should_free=0; \
+    char* subpath; \
+    subst(#f_name, path, &subpath, &should_free); \
+    ret_t retval = (*original_##f_name) farg; \
+    if(should_free) free(subpath); \
+    return retval;
 
 int open(const char *path, int flags, ...) {
     va_list args;
@@ -32,10 +52,7 @@ int open(const char *path, int flags, ...) {
     mode = va_arg (args, mode_t);
     va_end (args);
 
-    static int (*original_open)(const char*, int, mode_t);
-    if(!original_open)
-        original_open = dlsym(RTLD_NEXT, "open");
-    return (*original_open)(subst(path), flags, mode);
+    SUBST(int, open, (const char*, int, mode_t), (subpath, flags, mode), path)
 }
 
 int open64(const char *path, int flags, ...) {
@@ -46,22 +63,29 @@ int open64(const char *path, int flags, ...) {
     mode = va_arg (args, mode_t);
     va_end (args);
 
-    static int (*original_open)(const char*, int, mode_t);
-    if(!original_open)
-        original_open = dlsym(RTLD_NEXT, "open64");
-    return (*original_open)(subst(path), flags, mode);
+    SUBST(int, open64, (const char*, int, mode_t), (subpath, flags, mode), path)
 }
 
 FILE* fopen(const char *path, const char* mode) {
-    static FILE* (*original_open)(const char*, const char*);
-    if(!original_open)
-        original_open = dlsym(RTLD_NEXT, "fopen");
-    return (*original_open)(subst(path), mode);
+    SUBST(FILE*, fopen, (const char*, const char*), (subpath, mode), path)
 }
 
 FILE* fopen64(const char *path, const char* mode) {
-    static FILE* (*original_open)(const char*, const char*);
-    if(!original_open)
-        original_open = dlsym(RTLD_NEXT, "fopen64");
-    return (*original_open)(subst(path), mode);
+    SUBST(FILE*, fopen64, (const char*, const char*), (subpath, mode), path)
+}
+
+int stat(const char *path, void *buf) {
+    SUBST(int, stat, (const char*, void*), (subpath, buf), path)
+}
+
+int lstat(const char *path, void *buf) {
+    SUBST(int, lstat, (const char*, void*), (subpath, buf), path)
+}
+
+int stat64(const char *path, void *buf) {
+    SUBST(int, stat64, (const char*, void*), (subpath, buf), path)
+}
+
+int lstat64(const char *path, void *buf) {
+    SUBST(int, lstat64, (const char*, void*), (subpath, buf), path)
 }
